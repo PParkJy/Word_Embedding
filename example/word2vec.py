8172e90bdf83b7b8b262c22987629a4e406441db
Jiyeon Park <wldus8677@gmail.com>
2021-03-23 09:40:25 +0900

    Update word2vec.py

--- example/word2vec.py
+++ example/word2vec.py
@@ -1,9 +1,10 @@
 from nltk.tokenize import word_tokenize
 from gensim.models import Word2Vec
 from callback import callback
+from sklearn.manifold import TSNE
+import pandas as pd
+import matplotlib.pyplot as plt
 
-# Required at first time only
-# nltk.download('punkt')
 
 # Load data
 f = open("./dataset/preprocessed_data.txt", "r")
@@ -12,7 +13,6 @@ f.close()
 
 # Word tokenization
 tokenized_words = [word_tokenize(sentence[:-1]) for sentence in text] # remove "\n" by [:-1]
-#print(tokenized_words)
 
 # Word embedding parameter
 dim = 100
@@ -25,9 +25,28 @@ model = Word2Vec(sentences=tokenized_words, size=dim, window=window_size, sg=cbo
 
 # Print vector
 test = model.wv["world"]
-print(len(test))
+#print(test)) 
 
-# Similarity
-test = model.wv.most_similar(positive=["home"])
-print(test)
+# Cosine similarity
+test = model.wv.most_similar(positive=["happy"]) # negative option
+#print(test)
 
+# Check the distribution by TSNE
+vocab = list(model.wv.vocab)
+X = model[vocab]
+#print(len(X))
+
+tsne = TSNE(n_components=2)
+X_tsne = tsne.fit_transform(X[:200,:]) # select 100 words
+
+df = pd.DataFrame(X_tsne, index=vocab[:200], columns=['x', 'y'])
+#print(df.head(10))
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.scatter(df['x'], df['y'])
+
+for word, pos in df.iterrows():
+    ax.annotate(word, pos, fontsize=10)
+
+plt.show()
